114b6fb1a60a85e6c23fb9b9a91402f9fa0b36ab
Donbosco Kim <leonbosco2@gmail.com>
2024-03-10 16:44:53 -0500

    add MatCard, Expansion and Button Modules in app.module to solve 'mat-card is not a known element'

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +20,11 @@ import { HeaderComponent } from './head-create/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-app/src/app/post/post-file/post-file.component.ts
+++ my-app/src/app/post/post-file/post-file.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './post-file.component.css'
 })
 export class PostFileComponent {
+  newPostText = '';
+  posts: { text: string; timestamp: Date }[] = [];
 
+  submitPost() {
+    if (this.newPostText.trim() !== '') {
+      const newPost = {
+        text: this.newPostText,
+        timestamp: new Date()
+      };
+      this.posts.unshift(newPost); // Add new post to the beginning of the array
+      this.newPostText = ''; // Clear the textarea
+    }
+  }
 }
